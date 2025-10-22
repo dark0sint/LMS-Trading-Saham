ae3458669871802791dfcff31a4a6e162e6a2451
DARK OSINT <levianawulandari7@gmail.com>
2025-10-22 08:12:41 +0700

    Create Question.php

---
+++ Question.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Question extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['question', 'options', 'correct_answer', 'quiz_id'];
+
+    protected $casts = [
+        'options' => 'array',
+    ];
+
+    public function quiz()
+    {
+        return $this->belongsTo(Quiz::class);
+    }
+}
