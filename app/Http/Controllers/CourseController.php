4d784f722158d61263474484ae493ae59322d598
DARK OSINT <levianawulandari7@gmail.com>
2025-10-22 08:19:38 +0700

    Create CourseController.php

---
+++ app/Http/Controllers/CourseController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Course;
+use Illuminate\Http\Request;
+
+class CourseController extends Controller
+{
+    public function index()
+    {
+        $courses = Course::all();
+        return view('courses.index', compact('courses'));
+    }
+
+    public function show(Course $course)
+    {
+        return view('courses.show', compact('course'));
+    }
+
+    public function create()
+    {
+        return view('courses.create');
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'title' => 'required',
+            'description' => 'required',
+        ]);
+
+        Course::create([
+            'title' => $request->title,
+            'description' => $request->description,
+            'instructor_id' => auth()->id(),
+        ]);
+
+        return redirect()->route('courses.index');
+    }
+}
